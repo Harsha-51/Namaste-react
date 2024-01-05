6a68c380d9ec93783122a8e6b5eb8fd8c103f1ce
Harsha Surisetti <harsharoyalsurisetti@gmail.com>
2024-01-05 15:21:26 +0530

    episode-01

---
+++ inception/app.js
@@ -0,0 +1,5 @@
+const heading=React.createElement(
+    "div",{id:"parent"},[React.createElement("div",{id:"child1"},[React.createElement("h1",{},"Created using core fundamentals of react"),React.createElement("h2",{},"h2 element")]),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"Created using core fundamentals of react"),React.createElement("h2",{},"h2 element")])
+]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
